770e7eec290225bb910c43106edd2143f442101c
ichisei <132669382+ichisei@users.noreply.github.com>
2023-07-10 08:24:02 +0000

    [Update0710]ac_trader_search

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "customer/registrations",
+  sessions: 'custmer/sessions'
+}
+
+  devise_for :traders, skip: [:passwords], controllers: {
+  registrations: "trader/registrations",
+  sessions: "trader/sessions"
+}
+
+  root to: "homes#top"
+
+  # get 'homes/top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
