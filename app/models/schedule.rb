4f07919a712b61f93d5e74fd813cc461b0bf55b4
ichisei <132669382+ichisei@users.noreply.github.com>
2023-08-17 02:46:22 +0000

    [Update0817-2]ac_trader_search

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -8,7 +8,7 @@ class Schedule < ApplicationRecord
   validates :start_time, presence: true
 
   def date_before_start
-    errors.add(:start_time, "は過去の日付を選択できません") if start_time < Date.today
+    errors.add(:start_time, "過去の日付を選択できません") if start_time < Date.today
   end
 
 end
